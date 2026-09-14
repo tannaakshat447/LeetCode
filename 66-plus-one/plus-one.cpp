57426a8e2a4a333f9f9b26be7808d38ac6f19db4
Akshat Tanna <akshattanna4470@gmail.com>
2026-09-14 20:47:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (84.70%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        for (int i = digits.size() - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        // If all digits were 9
+        digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
