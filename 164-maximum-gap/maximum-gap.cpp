788f47416f8680dad9a5162f45ab281d96cc0e0b
Akshat Tanna <akshattanna4470@gmail.com>
2026-09-26 19:32:23 +0530

    Time: 43 ms (48.84%) | Memory: 85.7 MB (92.21%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size() < 2) return 0;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = abs(nums[0] - nums[1]);
+        for(int i=1;i<n-1;i++){
+            ans = max(ans, abs(nums[i]-nums[i+1]));
+        }
+
+        return ans;
+
+    }
+};
